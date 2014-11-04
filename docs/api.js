d1971cb79aaa1cf8ff295495f24a40feed21bd7e
Marcel Erz <erz@yahoo-inc.com>
2014-11-04 12:58:07 -0800

    Add API-documentation and Coverage report

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractClient",
+        "AbstractServer",
+        "BrowserStackServer",
+        "CabbieClient",
+        "ChromeDriverServer",
+        "Coverage",
+        "ExternalClient",
+        "ExternalServer",
+        "GhostDriverServer",
+        "SauceLabsServer",
+        "SeleniumServer",
+        "WebDriverClientDecorator",
+        "WebDriverContainer",
+        "WebDriverManager"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
